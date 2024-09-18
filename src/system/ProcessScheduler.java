f9bae9f6c1f8c91b64abc63c4994983707d9498a
HelenaMihaljcic <mihaljcichelena@gmail.com>
2024-09-18 17:57:54 +0200

    dodavanje procesa

---
+++ src/system/ProcessScheduler.java
@@ -0,0 +1,62 @@
+package system;
+
+import memory.Process;
+
+import java.util.PriorityQueue;
+
+public class ProcessScheduler extends Thread {
+    private static PriorityQueue<Process> red = new PriorityQueue<>((p1, p2) -> {
+        return Integer.compare((int) p1.getBurstTime(), (int) p2.getBurstTime());
+    });
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                // Uzmi proces sa najmanjim preostalim vremenom obrade
+                Process p = red.poll();
+
+
+                if (p == null) {
+                    // Ako nema procesa u redu, sačekaj
+                    Thread.sleep(100);
+                    continue;
+                }
+                if(p.getStanje() == ProcessState.READY){
+
+                }
+
+                if (p.getStanje() == ProcessState.TERMINATED) {
+                    continue;
+                }
+
+                p.setStanje(ProcessState.RUNNING);
+                p.start(); // Pokreni proces
+
+
+
+                if (p.getBurstTime() > 0) {
+                    // Ako proces nije završen, ponovo ga dodaj u red
+                    red.add(p);
+                }
+
+
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public static void addProcess(Process process) {
+        red.add(process);
+    }
+
+    public static void removeProcess(Process process) {
+        red.remove(process);
+    }
+
+    public PriorityQueue<Process> getRed() {
+        return red;
+    }
+}
